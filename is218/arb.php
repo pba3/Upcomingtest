454060771bb3274e1488549b25653627cd69378e
phinehas b alabi <richie>
2016-11-01 04:22:29 -0400

    argument added

---
+++ is218/arb.php
@@ -0,0 +1,18 @@
+<?php
+ function foo($arg1='sam', $arg2='tom'){
+
+ 	echo "arg1: $arg1\n";
+ 	echo "arg2: $arg2\n";
+}
+
+//text in each colon is printed to its arg.
+ foo('Hello','World');
+
+foo();
+//prints out arg1 and arg2 and they happen to sam and tom
+
+
+
+
+
+?>
