013ec368c54a6b3bfaef0f29286608e8dea099ab
phinehas b alabi <richie>
2016-12-08 20:14:51 -0500

    added class example

---
+++ basicApps218/classes/myexample.class.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+<form action="process.class.php" method ="post">
+	Enter cycle name <input name="name" type="text">
+	<input type="submit">
+</form>
+
+
+
+
+
+
+
+
+</body>
+</html>
