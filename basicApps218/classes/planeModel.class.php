c7a76956cc26ff5b2a51719c71795f2073e7b766
phinehas b alabi <richie>
2016-12-08 21:24:38 -0500

    added plane model

---
+++ basicApps218/classes/planeModel.class.php
@@ -0,0 +1,22 @@
+<?php
+
+class planeModel extends model{
+
+ private  $make;
+ private  $model;
+
+ public function setMake($make){
+  $this->make=$make;
+ }
+
+ public function getMake(){
+  return $this->make;
+
+ }
+ public function setModel($model){
+  $this->model=$model;
+ }
+ public function getModel(){
+  return $this->model;
+ }
+}
