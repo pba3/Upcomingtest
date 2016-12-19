a05007989d5031e4a6c116e50f08071b78fd6e37
phinehas b alabi <richie>
2016-12-18 22:15:57 -0500

    controller class

---
+++ is218/html/controller.class.php
@@ -0,0 +1,19 @@
+<?php
+
+abstract class controller{
+
+protected $html;
+public function get(){}
+public function post(){}
+public function put() {}
+public function delete() {}
+public function __construct() {
+        $header = 'put some header text here';
+	$this->html .= $header;
+	}
+	public function getHTML() {
+
+	return $this->html;
+	}
+	}
+?>
