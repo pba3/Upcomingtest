1ec0c398eef7700a8bf23d9858a77a6cf7d383b0
phinehas b alabi <richie>
2016-12-18 19:47:19 -0500

    just added autoload function

---
+++ is218/mvc.php
@@ -0,0 +1,13 @@
+<?php
+
+$model = new Model();
+$controller = new Controller($model);
+$view = new View($controller, $model);
+
+if (isset($_GET['action']) && !empty($_GET['action'])) {
+    $controller->{$_GET['action']}();
+    }
+
+    echo $view->output();
+
+ ?>
