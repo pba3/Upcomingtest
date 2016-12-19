2e862a2014033302af932c4c06cfef28215b9b89
phinehas b alabi <richie>
2016-12-18 20:46:30 -0500

    user profile

---
+++ is218/html/userprofile.class.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html>
+
+<body bgcolor="#E6E6FA">
+
+<title>Profile</title>
+<center>
+<header>
+<p><h2><i>Edit your profile</p></h2></i>
+<!--<form action ="upload.php" method="post" enctype="multipart/form-data">
+select image to upload:
+   <input type="file" name="fileToUpload" id="fileToUpload">
+      <input type="submit"value="Upload Image" name="submit">
+      </form>-->
+
+      <img src ="progamming.gif"alt="Computer man"  style ="width:1300px;height:128px;">
+      <form>
+       <br>
+         First Name:<br><input type ="text" name ="First Name"><br>
+	    <br>
+	        Last Name: <br><input type="text" name="Last Name"><br>
+		     <br>
+		           Your Email:<br><input type ="text" name ="Your Email"><br>
+			          <br>
+				          Password:<br>  <input type ="text" name ="Password"><br>
+					           <br>
+						             Confirm Password:<br>  <input type ="text" name ="Confirm
+							               Password"><br>
+								                  <br>
+										              Birthday:<br>  <input type ="text" name ="Birthday"><br>
+											                   <br>
+													                 <!-- <input type="button"
+															      onclick="alert"value="Submit"><br> -->
+
+															                  <br>
+																	   <center>
+																	                    <a
+																			    href="doneform.html">
+																			    Submit</a>
+																			     </center>
+																			      </form>
+																			       </html>
+																			        </body>
