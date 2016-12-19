d422d478cec2049220bd0de78679e9c026b67e2b
phinehas b alabi <richie>
2016-12-18 20:42:43 -0500

    login page

---
+++ is218/html/login.class.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login Page</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+	<div id="frm">
+		<form action="userprofile.class.php" method="POST">
+		  <p>
+		    <label> Username:</label>
+		    <input type="text" id="user" name="user" />
+		  </p>
+		  <p>
+		     <label>Password:</label>
+		     <input type="text" id="pas" name="pass" />
+		   </p>
+		   <p>
+		     <input type="submit" id="btn" value="Login" />
+		   </p>
+		</form>
+		</div>
+</body>
+</html>
+		  
+		    
