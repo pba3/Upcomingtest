76ee4b239d841e2656ca8d554ad13523ac0639b8
phinehas b alabi <richie>
2016-12-18 22:18:39 -0500

    autoload function

---
+++ is218/autoloader.php
@@ -0,0 +1,11 @@
+<?php
+  function my_autoloader($html){
+     include 'classes/'.$html.'.class.php';
+        }
+
+	   spl_autoload_register('my_autoloader');
+
+	      $signup = new signup;
+
+?>
+
