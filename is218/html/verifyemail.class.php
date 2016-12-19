9c5996897de6ef6078ae0f6fba474c7ddaff7b51
phinehas b alabi <richie>
2016-12-18 20:53:26 -0500

    verify email

---
+++ is218/html/verifyemail.class.php
@@ -0,0 +1,18 @@
+<DOCTYPE html>
+<html>
+<header>
+<i><h1><p> Thank you and Welcome!</i></h1></p>
+
+</header>
+<!-- <br>  <input type ="text" name ="That's Epic...Thank you for your Email
+     verificaition"><br> -->
+          <br>
+	       <!-- <input type="button" onclick="alert"value="Submit"><br> -->
+	             <br>
+		            <h3><a href="login.class.php">Proceed to
+			    Login</h3></a>
+
+
+
+			            </html>
+
