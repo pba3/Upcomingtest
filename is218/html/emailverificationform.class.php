501cffca822bd292104cc4e3b72a1e6e326f570e
phinehas b alabi <richie>
2016-12-18 20:28:38 -0500

    signup page

---
+++ is218/html/emailverificationform.class.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color:white;">
+<h1>Verify Your Email:</h1><br>  <input type ="text" name ="Verify Yur
+Email"><br>
+<br>
+
+<!-- <input type="button" onclick="alert"value="Submit"><br> -->
+ <br>
+   <a href ="verifyemail.html">Submit</a>
+
+
+
+
+
+      </body>
+         </html>
+
---
+++ is218/html/signup.class.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<header>
+<title>Welcome To singup Page</title>
+</header>
+
+<center>
+<i><h1><p>Welcome to Singup Page</i></h1></p>
+<form>
+<br>
+First Name:<br><input type="text" name="First Name"><br> <br>
+Last Name:<br><input type="text" name ="Last Name"<br>
+<br>
+Your Email:<br><input type ="text" name ="Your Email"><div
+    class="container" style="background-color:#FFFFFF">
+            <!-- <br><button type="button"
+		 class="cancelbtn">Cancel</button><br>
+		 -->
+		 <!-- <span class="psw">Forgot
+		      <a href="#">password?</a></span><br> -->
+<br>
+Password:<br>  <input type ="text" name ="Password"><br>
+<br>
+<a href="emailverificationform.html">Submit</a>
+</form>
+</center>
+
+
