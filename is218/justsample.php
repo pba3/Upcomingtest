54360729c2238110a2013eb08c10cbc07e289ebe
phinehas b alabi <richie>
2017-01-29 18:43:42 -0500

    just a sample code

---
+++ is218/justsample.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+
+<title>PHP</title>
+<body>
+ Hello!
+<form action='process.php' method='post' >
+Enter your name	<input name='name' type='text'>
+	<input type='submit'>
+</form>
+
+
+
+</body>
+</html>
