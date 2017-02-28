3d4f712bc084465a152c1ad4c7ace13b20d7a301
phinehas b alabi <richie>
2017-02-28 17:44:07 -0500

    update

--- download/xA1.php
+++ download/xA1.php
@@ -1,4 +1,8 @@
 <?php
+
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+	ini_set('display_erros', 1);
+	
 echo"Results from xA1.php with data from xA1.html";
 echo"<br>";
 
@@ -16,41 +20,36 @@ $pass=  $_GET["pass"];
 $amount=$_GET["amount"];
 $type=  $_GET["type"];
 
-print("name"."$name "."pass "."$pass"."amount "."$amount "."type "."$type ");
+print("user"."$name "."pass "."$pass"."amount "."$amount "."type "."$type ");
 echo"<br><br>";
 
-
 //protection
-$case=get_case($type, $name, $pass, $amount);//calling function
-print "$case";
-if ($case == 'A') {admin($name, $pass);};
-if ($case != 'A') {user($type, $name, $pass, $amount);};
+//get_case($type, $name, $pass, $amount);//calling function
 
-//print "$type";
-//print "<br><br>Entered user function.  ";
 
-//admin($name,$pass);
-$usr = user($type,$name,$pass,$amount);
-print "$usr";
+if ($type == 'A') {admin($name, $pass);};
+if ($type != 'A') {user($type, $name, $pass, $amount);};
+
+print "<br><br>Entered user function.  ";
+
+admin('admin', 007);
+user ( $type, $name, $pass, $amount  );
+
 print "<br><br>Passed user credentials and overwithdraw test.  Just a stub";
 print "<br>Entered admin function.";
 
-//print " $admin";
-
-//$user = user($type,$name,$pass,$amount);
 //if ($type == 'A') {admin($name, $pass);};
 //if ($type != 'A') {user($type, $name, $pass, $amount);};
-
 
 //update
 if($case != 'A'){update($type,$name,$amount);};
 print "<br> <br>This interaction is completed";
 
-mymail( $type, $name, $results1);
+//mymail( $type, $name, $results1);
 //$results1 = "hello";
 
-$results1= get_A($type, $s1);
-echo $results1;
+//$results1= get_A($type, $s1);
+//echo $results1;
 
 //$mail = mymail($to, $subject, $result1 , $headers);
 
