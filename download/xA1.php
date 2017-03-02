bf0a3944f7583f6dbb6f514de4bdb62dae995122
phinehas b alabi <richie>
2017-03-02 16:51:05 -0500

    newly made

--- download/xA1.php
+++ download/xA1.php
@@ -5,9 +5,9 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 	
 echo"Results from xA1.php with data from xA1.html";
 echo"<br>";
-
-include("xA1function.php");
 include ("account.php");
+include("xA1function.php");
+
 ($dbh = mysql_connect ( $hostname, $username, $password))
 	or die ( "Unable to connect to MySQL database" );
 
@@ -23,35 +23,33 @@ $type=  $_GET["type"];
 print("user"."$name "."pass "."$pass"."amount "."$amount "."type "."$type ");
 echo"<br><br>";
 
-//protection
-//get_case($type, $name, $pass, $amount);//calling function
 
+//protection
+$type= get_case($type, $name, $pass, $amount);//calling function
+print "<br>$type";
 
 if ($type == 'A') {admin($name, $pass);};
 if ($type != 'A') {user($type, $name, $pass, $amount);};
+if ($type !='A')  {update($type, $name, $amount);};
 
-print "<br><br>Entered user function.  ";
+sql( $type, $name, $s1, $s2 );//calling
+$result1= get_A($s1);
+$result2= get_T($s2);
+echo $result1.'<br>'.$result2;
+//$result1= get_A( $s1) . $result2=get_T($s2);
+if(isset($_GET['mailrequest'])){
+	mymail($type, $name, $result1.$result2);};
+//return $returnthis;
+//print "<br><br>Entered user function .  <br><br>";
+get_mail_address($type, $name);
+//user ( $type, $name, $pass, $amount  );
 
-admin('admin', 007);
-user ( $type, $name, $pass, $amount  );
 
-print "<br><br>Passed user credentials and overwithdraw test.  Just a stub";
-print "<br>Entered admin function.";
-
-//if ($type == 'A') {admin($name, $pass);};
-//if ($type != 'A') {user($type, $name, $pass, $amount);};
+//print "<br><br>Passed user credentials and overwithdraw test.  Just a stub";
+//print "<br>Entered admin function.";
 
 //update
-if($case != 'A'){update($type,$name,$amount);};
 print "<br> <br>This interaction is completed";
-
-//mymail( $type, $name, $results1);
-//$results1 = "hello";
-
-//$results1= get_A($type, $s1);
-//echo $results1;
-
-//$mail = mymail($to, $subject, $result1 , $headers);
 
 
 ?>
