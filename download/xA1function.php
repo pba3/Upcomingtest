7b077b1ed53db81c53fc31d6f1513a47f10285b2
phinehas b alabi <richie>
2017-03-02 16:51:36 -0500

    newly updated

--- download/xA1function.php
+++ download/xA1function.php
@@ -14,7 +14,7 @@ else if ($name=='' or $pass == ''){
 else if ($type=='A' and $amount !=''){
 	exit("Admin use other account when making deposites ");
 }
-else if ($type !='A' and $amount != ''){
+else if ($type !='A' and $amount == ''){
 	exit ("Enter an amount");
 }
 else if ($type !='A' and $amount != (is_numeric($amount))){
@@ -24,7 +24,7 @@ else if ($type !='A' and $amount <=0){
 	exit("Enter a positive number into amount ");
 }
 //else{
-	//return $type;
+	return $type;
 }
 
 
@@ -62,15 +62,15 @@ function update ($type, $name, $amount){
 //insert to T(Transacion)W. SQL is:
 $t = "insert into transaction values ('$name' ,'$type', '$amount', NOW() )";
 if ($type =="W"){
-	$s= "update account Set current_balance = current_balance - $amount where user = '$name' ";
+	$s= "UPDATE account SET current_balance = current_balance - $amount where user = '$name' ";
 
 } else if ($type =="D"){
 //update A(Account) is current_balance for D. SQL is:
-	$s= "update account Set current_balance = current_balance + $amount where user = '$name' ";
+	$s= "UPDATE account SET current_balance = current_balance + $amount where user = '$name' ";
 }
 	print("<br><br> SQL s for update is:<br>");
 	print("$s<br><br>");
-	print "Entered sql function. Just stub";
+	//print "Entered sql function. Just stub";
 	mysql_query($s);
 	($t=mysql_query($t)) or die(mysql_error());//return results
 	//($t=mysql_query($t)) or die (mysql_error());
@@ -78,7 +78,83 @@ if ($type =="W"){
 
 return;
 }
+function sql($type, $name, &$s1, &$s2){
+	if ( $type=='A') { $s1 = "select * from account  "; } ; 
+    if ( $type!='A') { $s1 = "select * from account where user = '$name' "; }; 
+    if ( $type=='A') { $s2 = "select * from transaction  "; } ; 
+    if ( $type!='A') { $s2 = "select * from transaction where user = '$name' "; }; 
+	print "$s2";
+}	
+	//$returnthis ="Accounts info: <br><br> $result1 <br><br><br> Transaction info: <br><br> $result2";
+	//return $returnthis;
 
+
+function get_A ($s1) {
+	$out =  "<br> \$s1 is:    $s1 <br>";
+	($t=mysql_query($s1)) or die(mysql_error());
+	while ($r=mysql_fetch_array($t)) {
+	$user =$r["user"];
+	$mail =$r["email"];
+	$fullname  = $r["full name"];
+	$address  = $r["address"];
+	$initial_balance  = $r["initial_balance"];
+	$current_balance  = $r["current_balance"];
+	$out  .="<br>user is $user";
+	$out  .="<br>mail is $email";
+	$out  .= "<br> User fullname is $fullname";
+	$out  .= "<br>Address is $address";
+	$out  .= "<br>Initial balance is $initial_balance";
+	$out  .= "<br>current is $current_balance";
+	
+	echo  "<br><br>Account info is: $result1";
+	return $out;
+	}
+}
+function get_T($s2) {
+	$out =  "<br> \$s2 is:    $s2 <br>";
+	($t=mysql_query($s2)) or die(mysql_error());
+	while ($r=mysql_fetch_array($t)) {
+	$user  =$r["user"];
+	$type  =$r["type"];
+	$amount=$r["amount"];
+	$data  =$r["data"];
+	if($type=='D'){
+		$out  .="<br>user deposited \$$amount on $data". ".";
+	} elseif ($type =='W'){
+		$out .="<br>user withdrew \$$amount on $data". ".";
+		}
+	}
+	//$out .="data is $data";
+	
+	//$out  .="<br>user is $user";
+	//echo  "<br><br>Transaction info is: $result2";
+	return $out;
+}
+function get_mail_address($type, $name){
+        if ($type == 'A'){
+            return 'greg1111@mailinator.com';
+        }
+        elseif ($type != 'A'){ 
+            $fetch_email = "select email from account WHERE user = '$name'";
+            ($fetch_email_query = mysql_query($fetch_email) or die(mysql_error()));
+            $row = mysql_fetch_array($fetch_email_query);
+            $emailing = $row["email"];
+			echo $emailing;
+            return $emailing;
+        }
+    }
+
+function mymail($type, $name, $message){
+	$to = 'greg1111@mailinator.com';
+	$subject = 'account & transaction table? ';
+	//$headers = "From:abc@somebody.com \r\n";
+	//$message = "Hello how are you<br> Phin";
+	$headers ='MIME-Version: 1.0';
+	$headers .='Content-type: text/html; charset=iso-8859-1';
+	echo('<br>	Mail is sent<br><br>');
+	mail($to, $subject, $message, $headers);
+
+}
 
 
 ?>
